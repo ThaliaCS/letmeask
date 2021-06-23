bed2eae4a3852eea47aa7f8d4431ced24a40bb05
ThaliaCS <thalia.schone@gmail.com>
2021-06-23 13:39:04 -0300

    added context added login with google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,24 @@
 import {useHistory} from 'react-router-dom';
 
-import {auth, firebase} from '../services/firebase';
-
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import '../styles/auth.scss';
+
 import Button from "../components/Button";
+import { useAuth } from '../hooks/useAuth';
 
 export default function Home() {
 
   const history = useHistory();
+  const { signInWithGoogle, user} = useAuth();
 
-  function signIn(){
+  async function handleCreateRoom(){
 
-  }
-
-  function handleCreateRoom(){
-
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-
+    if(!user){
+     await signInWithGoogle();
+    }
       history.push('/rooms/new'); 
-    });
-
   }
 
   return (
